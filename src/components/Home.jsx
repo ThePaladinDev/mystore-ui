e16b81c011c9c6744a7854d02cf61adec0a19ff2
ThePaladinDev <133822824+ThePaladinDev@users.noreply.github.com>
2026-04-17 12:39:31 +0530

    Add api to fetch products from backend

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,20 @@
 import PageHeading from './PageHeading';
 import ProductListings from './ProductListings';
-import products from '../data/products';
+import { useEffect, useState } from 'react';
+import apiClient from '../api/apiClient';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const response = await apiClient.get('/products');
+    setProducts(response.data);
+  };
+
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Welcome">
