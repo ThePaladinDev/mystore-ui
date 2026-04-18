87a166bf1b33b4ac5860fc61af42fe7c01382937
ThePaladinDev <133822824+ThePaladinDev@users.noreply.github.com>
2026-04-18 19:00:52 +0530

    Create components for various pages

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,21 @@ import {
 } from 'react-router';
 import Home from './components/Home.jsx';
 import Error from './pages/Error.jsx';
+import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
+import Login from './components/Login.jsx';
+import Cart from './components/Cart.jsx';
+import ProductDetail from './components/ProductDetail.jsx';
 
 const routeDefinitions = createRoutesFromElements(
   <Route path="/" element={<App />} errorElement={<Error />}>
     <Route index element={<Home />} />
     <Route path="/home" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/products/:productId" element={<ProductDetail />} />
   </Route>,
 );
 
